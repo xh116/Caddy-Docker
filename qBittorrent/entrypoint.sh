bb49927fcc54d34824501da3db58f0e13b89b784
xh116 <37950502+xh116@users.noreply.github.com>
2021-12-29 06:43:44 +0800

    Update entrypoint.sh

--- qBittorrent/entrypoint.sh
+++ qBittorrent/entrypoint.sh
@@ -1,10 +1,37 @@
 #!/bin/sh -e
 
 # Default configuration file
-if [ ! -f /config/qBittorrent.conf ]
-then
-	cp /default/qBittorrent.conf /config/qBittorrent.conf
+WEBUI_PORT=${WEBUI_PORT:-8080}
+ALT_WEBUI=${ALT_WEBUI:-false}
+if [ "${ALT_WEBUI}" != "true" ]; then
+  ALT_WEBUI=false
 fi
+
+
+if [ ! -f /config/qBittorrent.conf ]; then
+  echo "Initializing qBittorrent configuration..."
+  cat > /config/qBittorrent.conf <<EOL
+
+[Preferences]
+General\Locale=C
+WebUI\Enabled=true
+WebUI\Address=*
+WebUI\ServerDomains=*
+WebUI\AlternativeUIEnabled=${ALT_WEBUI}
+WebUI\Port=${WEBUI_PORT}
+Downloads\SavePath=/downloads
+Connection\PortRangeMin=9881
+Downloads\ScanDirsV2=@Variant(\0\0\0\x1c\0\0\0\0)
+
+[LegalNotice]
+Accepted=true
+
+[General]
+ported_to_new_savepath_system=true
+
+EOL
+fi
+
 
 # Allow groups to change files.
 umask 002
