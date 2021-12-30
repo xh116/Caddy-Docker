fa1186489d0aff7ace1db54edc0803d840012048
xh116 <37950502+xh116@users.noreply.github.com>
2021-12-31 01:43:22 +0800

    Update entrypoint.sh

--- qBittorrent/entrypoint.sh
+++ qBittorrent/entrypoint.sh
@@ -14,10 +14,6 @@ if [ ! -f /config/qBittorrent.conf ]; then
 [General]
 ported_to_new_savepath_system=true
 
-[Application]
-FileLogger\Enabled=true
-FileLogger\Path=/var/log/qbittorrent
-
 [LegalNotice]
 Accepted=true
 
@@ -33,14 +29,10 @@ WebUI\Enabled=true
 WebUI\Address=*
 WebUI\Port=${WEBUI_PORT}
 WebUI\AlternativeUIEnabled=${ALT_WEBUI}
-
-
 EOL
 fi
 
 echo "Overriding required parameters..."
-sed -i "s!FileLogger\\\Enabled=.*!FileLogger\\\Enabled=true!g"  /config/qBittorrent.conf
-sed -i "s!FileLogger\\\Path=.*!FileLogger\\\Path=/var/log/qbittorrent!g"  /config/qBittorrent.conf
 sed -i "s!ported_to_new_savepath_system.*!ported_to_new_savepath_system=true!g"  /config/qBittorrent.conf
 sed -i "s!Downloads\\\SavePath=.*!Downloads\\\SavePath=/downloads!g" /config/qBittorrent.conf
 sed -i "s!Downloads\\\TempPath=.*!Downloads\\\TempPath=/downloads/incomplete!g"  /config/qBittorrent.conf
@@ -53,8 +45,7 @@ chown qbittorrent:qbittorrent  /config \
 /data \
 /downloads 
 
-chown -R qbittorrent:qbittorrent "${HOME}" /var/log/qbittorrent
-
+chown -R qbittorrent:qbittorrent "${HOME}" 
 # Allow groups to change files.
 umask 002
   
