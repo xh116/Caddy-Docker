7159f924b5ec97fea73047efd18f98ccb6ecbaf3
xh116 <37950502+xh116@users.noreply.github.com>
2021-12-30 19:02:38 +0800

    Update entrypoint.sh

--- qBittorrent/entrypoint.sh
+++ qBittorrent/entrypoint.sh
@@ -1,12 +1,5 @@
 #!/bin/sh -e
 
-# Default configuration file
-WEBUI_PORT=${WEBUI_PORT:-8080}
-ALT_WEBUI=${ALT_WEBUI:-false}
-if [ "${ALT_WEBUI}" != "true" ]; then
-  ALT_WEBUI=false
-fi
-
 
 if [ ! -f /config/qBittorrent.conf ]; then
   echo "Initializing qBittorrent configuration..."
@@ -15,20 +8,14 @@ if [ ! -f /config/qBittorrent.conf ]; then
 [General]
 ported_to_new_savepath_system=true
 
-[BitTorrent]
-Session\Port=59881
-Session\QueueingSystemEnabled=true
-
 [Preferences]
 WebUI\Enabled=true
 WebUI\Address=*
 WebUI\ServerDomains=*
-WebUI\AlternativeUIEnabled=${ALT_WEBUI}
 WebUI\Port=${WEBUI_PORT}
 Downloads\SavePath=/downloads
 Downloads\TempPath=/downloads/incomplete
 Downloads\TempPathEnabled=true
-Downloads\ScanDirsV2=@Variant(\0\0\0\x1c\0\0\0\0)
 
 [LegalNotice]
 Accepted=true
@@ -36,6 +23,7 @@ Accepted=true
 EOL
 fi
 
+sed -i "s!ported_to_new_savepath_system.*!ported_to_new_savepath_system=true!g"  /config/qBittorrent.conf
 sed -i "s!WebUI\\\Port=.*!WebUI\\\Port=${WEBUI_PORT}!g"  /config/qBittorrent.conf
 sed -i "s!Downloads\\\SavePath=.*!Downloads\\\SavePath=/downloads!g" /config/qBittorrent.conf
 sed -i "s!Downloads\\\TempPath=.*!Downloads\\\TempPath=/downloads/incomplete!g"  /config/qBittorrent.conf
