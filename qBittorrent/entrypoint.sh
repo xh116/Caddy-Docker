5aab520145cc9fd8742727f06106f7b2b674291e
xh116 <37950502+xh116@users.noreply.github.com>
2022-01-03 15:10:40 +0800

    Update entrypoint.sh

--- qBittorrent/entrypoint.sh
+++ qBittorrent/entrypoint.sh
@@ -10,23 +10,86 @@ if [ -n "${PUID}" ] && [ "${PUID}" != "$(id -u qbittorrent)" ]; then
   sed -i -e "s/^qbittorrent:\([^:]*\):[0-9]*:\([0-9]*\)/qbittorrent:\1:${PUID}:\2/" /etc/passwd
 fi
 
-if [ ! -f  /home/qbittorrent/.config/qBittorrent/qBittorrent.conf ]; then
+if [ ! -f  /config/qBittorrent.conf ]; then
   echo "Initializing qBittorrent configuration..."
-  cat >  /home/qbittorrent/.config/qBittorrent/qBittorrent.conf <<EOL  
+  cat >  /config/qBittorrent.conf <<EOL  
 [General]
 ported_to_new_savepath_system=true
+
+[AutoRun]
+enabled=false
+program=
+
+[BitTorrent]
+Session\DefaultSavePath=/downloads
+Session\Port=9881
+Session\QueueingSystemEnabled=true
+Session\TempPath=/downloads/incomplete
+Session\TempPathEnabled=true
+
+[Core]
+AutoDeleteAddedTorrentFile=Never
+
 [LegalNotice]
 Accepted=true
+
+[Meta]
+MigrationVersion=2
+
+[Network]
+Proxy\OnlyForTorrents=false
+
 [Preferences]
+Advanced\RecheckOnCompletion=false
+Advanced\trackerPort=9000
 Connection\PortRangeMin=6881
+Connection\ResolvePeerCountries=true
 Downloads\SavePath=/downloads
 Downloads\TempPath=/downloads/incomplete
 Downloads\TempPathEnabled=true
+DynDNS\DomainName=changeme.dyndns.org
+DynDNS\Enabled=false
+DynDNS\Password=
+DynDNS\Service=DynDNS
+DynDNS\Username=
+General\Locale=
+MailNotification\email=
+MailNotification\enabled=false
+MailNotification\password=
+MailNotification\req_auth=true
+MailNotification\req_ssl=false
+MailNotification\sender=qBittorrent_notification@example.com
+MailNotification\smtp_server=smtp.changeme.com
+MailNotification\username=
 Queueing\QueueingEnabled=true
 WebUI\Address=*
+WebUI\AlternativeUIEnabled=false
+WebUI\AuthSubnetWhitelist=@Invalid()
+WebUI\AuthSubnetWhitelistEnabled=false
+WebUI\BanDuration=3600
+WebUI\CSRFProtection=true
+WebUI\ClickjackingProtection=true
+WebUI\CustomHTTPHeaders=
+WebUI\CustomHTTPHeadersEnabled=false
 WebUI\Enabled=true
-WebUI\Port=8080
+WebUI\HTTPS\CertificatePath=
+WebUI\HTTPS\Enabled=false
+WebUI\HTTPS\KeyPath=
+WebUI\HostHeaderValidation=true
+WebUI\LocalHostAuth=true
+WebUI\MaxAuthenticationFailCount=5
+WebUI\ReverseProxySupportEnabled=false
+WebUI\RootFolder=
+WebUI\SecureCookie=true
 WebUI\ServerDomains=*
+WebUI\SessionTimeout=3600
+WebUI\TrustedReverseProxiesList=
+WebUI\UseUPnP=true
+WebUI\Username=admin
+
+[RSS]
+AutoDownloader\DownloadRepacks=true
+AutoDownloader\SmartEpisodeFilter=s(\\d+)e(\\d+), (\\d+)x(\\d+), "(\\d{4}[.\\-]\\d{1,2}[.\\-]\\d{1,2})", "(\\d{1,2}[.\\-]\\d{1,2}[.\\-]\\d{4})"
 EOL
 fi
 
