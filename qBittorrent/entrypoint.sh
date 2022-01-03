7c437466191f91c445d60072a5c5022bae7d56a1
xh116 <37950502+xh116@users.noreply.github.com>
2022-01-03 15:50:24 +0800

    Update entrypoint.sh

--- qBittorrent/entrypoint.sh
+++ qBittorrent/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh -e
 
+# base on https://github.com/crazy-max/docker-qbittorrent/blob/master/entrypoint.sh
+
 if [ -n "${PGID}" ] && [ "${PGID}" != "$(id -g qbittorrent)" ]; then
   echo "Switching to PGID ${PGID}..."
   sed -i -e "s/^qbittorrent:\([^:]*\):[0-9]*/qbittorrent:\1:${PGID}/" /etc/group
@@ -22,7 +24,7 @@ program=
 
 [BitTorrent]
 Session\DefaultSavePath=/downloads
-Session\Port=9881
+Session\Port=6881
 Session\QueueingSystemEnabled=true
 Session\TempPath=/downloads/incomplete
 Session\TempPathEnabled=true
@@ -36,60 +38,23 @@ Accepted=true
 [Meta]
 MigrationVersion=2
 
-[Network]
-Proxy\OnlyForTorrents=false
-
 [Preferences]
-Advanced\RecheckOnCompletion=false
-Advanced\trackerPort=9000
 Connection\PortRangeMin=6881
 Connection\ResolvePeerCountries=true
 Downloads\SavePath=/downloads
 Downloads\TempPath=/downloads/incomplete
 Downloads\TempPathEnabled=true
-DynDNS\DomainName=changeme.dyndns.org
-DynDNS\Enabled=false
-DynDNS\Password=
-DynDNS\Service=DynDNS
-DynDNS\Username=
 General\Locale=
-MailNotification\email=
-MailNotification\enabled=false
-MailNotification\password=
-MailNotification\req_auth=true
-MailNotification\req_ssl=false
-MailNotification\sender=qBittorrent_notification@example.com
-MailNotification\smtp_server=smtp.changeme.com
-MailNotification\username=
 Queueing\QueueingEnabled=true
 WebUI\Address=*
 WebUI\AlternativeUIEnabled=false
-WebUI\AuthSubnetWhitelist=@Invalid()
-WebUI\AuthSubnetWhitelistEnabled=false
-WebUI\BanDuration=3600
-WebUI\CSRFProtection=true
-WebUI\ClickjackingProtection=true
-WebUI\CustomHTTPHeaders=
-WebUI\CustomHTTPHeadersEnabled=false
 WebUI\Enabled=true
-WebUI\HTTPS\CertificatePath=
-WebUI\HTTPS\Enabled=false
-WebUI\HTTPS\KeyPath=
 WebUI\HostHeaderValidation=true
 WebUI\LocalHostAuth=true
-WebUI\MaxAuthenticationFailCount=5
-WebUI\ReverseProxySupportEnabled=false
 WebUI\RootFolder=
 WebUI\SecureCookie=true
 WebUI\ServerDomains=*
-WebUI\SessionTimeout=3600
-WebUI\TrustedReverseProxiesList=
 WebUI\UseUPnP=true
-WebUI\Username=admin
-
-[RSS]
-AutoDownloader\DownloadRepacks=true
-AutoDownloader\SmartEpisodeFilter=s(\\d+)e(\\d+), (\\d+)x(\\d+), "(\\d{4}[.\\-]\\d{1,2}[.\\-]\\d{1,2})", "(\\d{1,2}[.\\-]\\d{1,2}[.\\-]\\d{4})"
 EOL
 fi
 
