286225af090f72be5b61731a5e8179c1812547f5
xh116 <37950502+xh116@users.noreply.github.com>
2022-03-13 15:41:47 +0800

    Update caddy.sh

--- caddy.sh
+++ caddy.sh
@@ -287,9 +287,8 @@ function install_caddy() {
         }
 }
 :443, ${CONF_DOMAIN} {
-        tls admin@tinyserve.com {
+        tls ${CONF_EMAIL} {
                 protocols tls1.2 tls1.3
-                ciphers TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256 TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256 TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256 TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256
         }
         route {
                 trojan {
